943c22fb150080768d07b2d9fc504443f11eedc5
baebae00 <bbae0413@naver.com>
2021-06-18 01:12:16 +0900

    join

---
+++ app/src/main/java/com/example/sswusw/SingUpActivity.java
@@ -0,0 +1,77 @@
+package com.example.sswusw;
+
+import android.os.Bundle;
+import android.text.TextWatcher;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.EditText;
+import android.text.Editable;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+public class SingUpActivity extends AppCompatActivity {
+
+    private ArrayAdapter collegeadapter;
+    private Spinner collegespinner;
+
+    private ArrayAdapter majoradapter;
+    private Spinner majorspinner;
+
+    EditText passwordEditText, passwordCheckEditText, correctEditText;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_sign_up);
+        findViewById(R.id.signUpButton).setOnClickListener(onClickListener);
+
+        // 단과대 스크롤
+        collegespinner = (Spinner) findViewById(R.id.collegeSpinner);
+        collegeadapter = ArrayAdapter.createFromResource(this, R.array.college, android.R.layout.simple_spinner_dropdown_item);
+        collegespinner.setAdapter(collegeadapter);
+
+        // 학과 스크롤
+        majorspinner = (Spinner) findViewById(R.id.majorSpinner);
+        majoradapter = ArrayAdapter.createFromResource(this, R.array.major, android.R.layout.simple_spinner_dropdown_item);
+        majorspinner.setAdapter(majoradapter);
+
+        passwordEditText = (EditText)findViewById(R.id.passwordEditText);
+        passwordCheckEditText = (EditText)findViewById(R.id.passwordCheckEditText);
+        correctEditText = (EditText) findViewById(R.id.correctEditText);
+        passwordCheckEditText.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+            }
+
+            @Override
+            public void afterTextChanged(Editable editable) {
+                if (passwordEditText.getText().toString().equals(passwordCheckEditText.getText().toString())) {
+                    // correct
+                    correctEditText.setText("일치합니다.");
+                } else {
+                    // incorrect
+                    correctEditText.setText("일치하지 않습니다.");
+                }
+            }
+        });
+
+    }
+
+    View.OnClickListener onClickListener = new View.OnClickListener() {
+        @Override
+        public void onClick(View v) {
+            switch (v.getId()) {
+                case R.id.signUpButton:
+                    Log.e("클릭", "클릭");
+                    break;
+            }
+        }
+    };
+}
