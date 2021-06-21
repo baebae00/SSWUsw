a6497b90c244304bd0f6ceacb9dcced8069a261e
baebae00 <bbae0413@naver.com>
2021-06-21 12:20:59 +0900

    blank check

--- app/src/main/java/com/example/sswusw/SingUpActivity.java
+++ app/src/main/java/com/example/sswusw/SingUpActivity.java
@@ -9,6 +9,7 @@ import android.widget.Spinner;
 import android.widget.EditText;
 import android.text.Editable;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class SingUpActivity extends AppCompatActivity {
@@ -18,6 +19,8 @@ public class SingUpActivity extends AppCompatActivity {
 
     private ArrayAdapter majoradapter;
     private Spinner majorspinner;
+
+    private AlertDialog dialog;
 
     EditText emailEditText, nicknameEditText, idEditText, passwordEditText, passwordCheckEditText, correctEditText, studentNumberEditText, contestCountEditText;
 
@@ -76,12 +79,69 @@ public class SingUpActivity extends AppCompatActivity {
             String StudentNumber = studentNumberEditText.getText().toString();
             String ContestCount = contestCountEditText.getText().toString();
 
-            switch (v.getId())
+            /* switch (v.getId())
             {
                 case R.id.signUpButton:
                     Log.e("클릭", "클릭");
                     break;
+            }*/
+
+            //아이디 중복체크 했는지 확인
+            /* if (!validate) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                dialog = builder.setMessage("중복된 아이디가 있는지 확인하세요.").setNegativeButton("확인", null).create();
+                dialog.show();
+                return;
+            } */
+
+            //한 칸이라도 입력 안했을 경우
+            if (userEmail.equals("") || userNickname.equals("") || userId.equals("") || userPassword.equals("") || userPasswordCheck.equals("") ||
+                    StudentNumber.equals("") || ContestCount.equals("")) {
+                AlertDialog.Builder builder = new AlertDialog.Builder(SingUpActivity.this);
+                dialog = builder.setMessage("모두 입력해주세요.").setNegativeButton("확인", null).create();
+                dialog.show();
+                return;
             }
+
+           /*  Response.Listener<String> responseListener = new Response.Listener<String>() {
+                @Override
+                public void onResponse(String response) {
+
+                    try {
+                        JSONObject jsonObject = new JSONObject( response );
+                        boolean success = jsonObject.getBoolean( "success" );
+
+                        //회원가입 성공시
+                        if(UserPwd.equals(PassCk)) {
+                            if (success) {
+
+                                Toast.makeText(getApplicationContext(), String.format("%s님 가입을 환영합니다.", UserName), Toast.LENGTH_SHORT).show();
+                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
+                                startActivity(intent);
+
+                                //회원가입 실패시
+                            } else {
+                                Toast.makeText(getApplicationContext(), "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show();
+                                return;
+                            }
+                        } else {
+                            AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
+                            dialog = builder.setMessage("비밀번호가 동일하지 않습니다.").setNegativeButton("확인", null).create();
+                            dialog.show();
+                            return;
+                        }
+
+                    } catch (JSONException e) {
+                        e.printStackTrace();
+                    }
+
+                }
+            };
+
+            //서버로 Volley를 이용해서 요청
+            RegisterRequest registerRequest = new RegisterRequest( UserEmail, UserPwd, UserName, responseListener);
+            RequestQueue queue = Volley.newRequestQueue( RegisterActivity.this );
+            queue.add( registerRequest ); */
         }
     };
 }
