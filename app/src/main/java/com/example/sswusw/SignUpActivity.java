689d2ab7596b8734a6e8f09517748ae4711689ca
baebae00 <bbae0413@naver.com>
2021-06-21 14:47:16 +0900

    blank check

--- app/src/main/java/com/example/sswusw/SignUpActivity.java
+++ app/src/main/java/com/example/sswusw/SignUpActivity.java
@@ -5,14 +5,16 @@ import android.text.TextWatcher;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.Button;
 import android.widget.Spinner;
 import android.widget.EditText;
 import android.text.Editable;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
-public class SingUpActivity extends AppCompatActivity {
+public class SignUpActivity extends AppCompatActivity {
 
     private ArrayAdapter collegeadapter;
     private Spinner collegespinner;
@@ -70,7 +72,6 @@ public class SingUpActivity extends AppCompatActivity {
     View.OnClickListener onClickListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-
             String userEmail = emailEditText.getText().toString();
             String userNickname = nicknameEditText.getText().toString();
             String userId = idEditText.getText().toString();
@@ -79,69 +80,11 @@ public class SingUpActivity extends AppCompatActivity {
             String StudentNumber = studentNumberEditText.getText().toString();
             String ContestCount = contestCountEditText.getText().toString();
 
-            /* switch (v.getId())
-            {
+            switch (v.getId()) {
                 case R.id.signUpButton:
                     Log.e("클릭", "클릭");
                     break;
-            }*/
-
-            //아이디 중복체크 했는지 확인
-            /* if (!validate) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                dialog = builder.setMessage("중복된 아이디가 있는지 확인하세요.").setNegativeButton("확인", null).create();
-                dialog.show();
-                return;
-            } */
-
-            //한 칸이라도 입력 안했을 경우
-            if (userEmail.equals("") || userNickname.equals("") || userId.equals("") || userPassword.equals("") || userPasswordCheck.equals("") ||
-                    StudentNumber.equals("") || ContestCount.equals("")) {
-                AlertDialog.Builder builder = new AlertDialog.Builder(SingUpActivity.this);
-                dialog = builder.setMessage("모두 입력해주세요.").setNegativeButton("확인", null).create();
-                dialog.show();
-                return;
             }
-
-           /*  Response.Listener<String> responseListener = new Response.Listener<String>() {
-                @Override
-                public void onResponse(String response) {
-
-                    try {
-                        JSONObject jsonObject = new JSONObject( response );
-                        boolean success = jsonObject.getBoolean( "success" );
-
-                        //회원가입 성공시
-                        if(UserPwd.equals(PassCk)) {
-                            if (success) {
-
-                                Toast.makeText(getApplicationContext(), String.format("%s님 가입을 환영합니다.", UserName), Toast.LENGTH_SHORT).show();
-                                Intent intent = new Intent(RegisterActivity.this, LoginActivity.class);
-                                startActivity(intent);
-
-                                //회원가입 실패시
-                            } else {
-                                Toast.makeText(getApplicationContext(), "회원가입에 실패하였습니다.", Toast.LENGTH_SHORT).show();
-                                return;
-                            }
-                        } else {
-                            AlertDialog.Builder builder = new AlertDialog.Builder(RegisterActivity.this);
-                            dialog = builder.setMessage("비밀번호가 동일하지 않습니다.").setNegativeButton("확인", null).create();
-                            dialog.show();
-                            return;
-                        }
-
-                    } catch (JSONException e) {
-                        e.printStackTrace();
-                    }
-
-                }
-            };
-
-            //서버로 Volley를 이용해서 요청
-            RegisterRequest registerRequest = new RegisterRequest( UserEmail, UserPwd, UserName, responseListener);
-            RequestQueue queue = Volley.newRequestQueue( RegisterActivity.this );
-            queue.add( registerRequest ); */
         }
     };
 }
